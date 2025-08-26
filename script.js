0ab108303153d54af6b969da664eeea98586949d
alexnoy <alexander.noy123@gmail.com>
2025-08-26 00:04:55 -0400

    Modify displayBook function
    the function needed to check to see if the book already exists in the DOM as when a new book was created it would add the books that already existed on the page once again.

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ function displayBook() {
             continue;
         } else {
             const card = document.createElement('div');
-            card.setAttribute('data-id', book.id);
             card.classList.add('books');
             for (const info in book) {
                 if (info === 'id') {
